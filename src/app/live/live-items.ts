129b2f394bb985415380d8c423d09cd4dad9799c
eyvindwa <eyvindwa@gmail.com>
2025-07-23 12:42:13 +0200

    Update live-items.ts

--- src/app/live/live-items.ts
+++ src/app/live/live-items.ts
@@ -1,26 +1,27 @@
 // tslint:disable:max-line-length
 
 export const Upcoming = [
-  {
-    date: '29.05.2025',
-    description: 'Last Train, Oslo, with Repulsive Feast and Contorted',
-    link: 'https://www.facebook.com/events/1700026431398844/',
-    poster: 'assets/img/posters/Malevolent-May-ODF-2025.jpg'
-  },
-  {
-    date: '06.06.2025',
-    description: 'Finnish Death Metal Maniacs Festival 2025',
-    link: 'https://www.facebook.com/events/452679484105699',
-    poster: 'assets/img/posters/FDMM-2025.jpg'
-  },
-  {
-    date: '07.06.2025',
-    description: 'Tallinn, TBA'
-  }
+
 ];
 
 export const Past = [
-    {
+  {
+        date: '07.06.2025',
+        description: 'Tallinn, The Krypt'
+  },
+  {
+        date: '06.06.2025',
+        description: 'Finnish Death Metal Maniacs Festival 2025',
+        link: 'https://www.facebook.com/events/452679484105699',
+        poster: 'assets/img/posters/FDMM-2025.jpg'
+  },
+  {
+        date: '29.05.2025',
+        description: 'Last Train, Oslo, with Repulsive Feast and Contorted',
+        link: 'https://www.facebook.com/events/1700026431398844/',
+        poster: 'assets/img/posters/Malevolent-May-ODF-2025.jpg'
+   },
+   {
         date: '25-28.09.2024',
         description: 'Legend Metalfest Trondheim 2024',
         link: 'https://www.facebook.com/legendmetalfest/',
